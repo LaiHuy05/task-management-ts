121bbf42ec1fb4d010c503e00f5eafc9c127fecf
HUY <huy20050416@gmail.com>
2026-08-10 19:58:27 +0700

    tao route

---
+++ api/v1/routes/index-route.ts
@@ -0,0 +1,11 @@
+import { Express } from "express";
+import { taskRoute } from "./task-route";
+
+
+const mainV1Routes = (app: Express): void => {
+  const version = "/api/v1";
+
+  app.use(version + "/tasks", taskRoute);
+
+};
+export default mainV1Routes;
